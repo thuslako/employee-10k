e5d771ab822ffd9db66ee0fe65a0ad0cb3832497
Luk <lukundu.lako@gmail.com>
2022-01-14 00:55:18 -0500

    update: upload middleware setup - package updates

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,11 +1,22 @@
 const multer = require('multer')
 const grid = require('multer-gridfs-storage')
+const crypto = require('crypto')
 
 const storage = new grid({
  url : process.env.MDB_URI, 
- file: async (req,file) => {
-  //  set image format and handle preupload checks 
+ cache: true,
+ file: (req,file) => {
+   return new Promise((resolve, reject)=>{
+      crypto.randomBytes(8, (error, buffer) =>{
+        if(error) return reject(error)
+        const encryptedName = `${buffer.toString('hex')}-${Date.now()}`
+        resolve({
+          fileName: encryptedName,
+          bucketName: 'uploads'
+        })
+      })
+   })
  }
 })
 
-module.exports = storage
+module.exports = multer({storage})
